9996ec91edce87014dd3e49aed5075e2158d286d
JosueGarciaBetancourt <garciabetancourtjosuedaniel@gmail.com>
2025-03-14 14:26:44 -0500

    e

--- app/Models/PerfilUsuario.php
+++ app/Models/PerfilUsuario.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Carbon\Carbon;
 
 class PerfilUsuario extends Model
 {
@@ -18,8 +19,27 @@ class PerfilUsuario extends Model
     protected $fillable = [
         'idPerfilUsuario',
         'nombre_PerfilUsuario',
+        'created_at',
+        'updated_at',
     ];
 
+    // Mutador para created_at
+    protected static function boot()
+    {
+        parent::boot();
+
+        // Ajuste de hora al crear un registro
+        static::creating(function ($perfilUsuario) {
+            $perfilUsuario->created_at = Carbon::now()->addHours(5);
+            $perfilUsuario->updated_at = Carbon::now()->addHours(5);
+        });
+
+        // Ajuste de hora al actualizar un registro
+        static::updating(function ($perfilUsuario) {
+            $perfilUsuario->updated_at = Carbon::now()->addHours(5);
+        });
+    }
+      
     public function users()
     {
         return $this->hasMany(User::class, 'idPerfilUsuario', 'id'); 
--- database/migrations/0001_01_01_000005_create_perfilesUsuarios_table.php
+++ database/migrations/0001_01_01_000005_create_perfilesUsuarios_table.php
@@ -12,10 +12,8 @@ return new class extends Migration
         Schema::create('PerfilesUsuarios', function (Blueprint $table) {
             $table->id("idPerfilUsuario");
             $table->string('nombre_PerfilUsuario', 50);
-            /* $table->timestamps(); //created_at updated_at */
+            $table->timestamps(); //created_at updated_at
             // Definir created_at con CURRENT_TIMESTAMP por defecto
-            $table->timestamp('created_at')->useCurrent();
-            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate(); 
             /* $table->timestamp('created_at')->default(DB::raw('(CURRENT_TIMESTAMP + INTERVAL 5 HOUR)'));
             $table->timestamp('updated_at')->nullable(); */
             $table->softDeletes(); //deleted_at
