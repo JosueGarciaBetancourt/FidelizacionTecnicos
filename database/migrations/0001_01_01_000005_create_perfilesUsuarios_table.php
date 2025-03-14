9ed40937cfe13c2c4e266e3db5d5fa56c36482c7
JosueGarciaBetancourt <garciabetancourtjosuedaniel@gmail.com>
2025-03-14 13:49:46 -0500

    a

--- database/migrations/0001_01_01_000005_create_perfilesUsuarios_table.php
+++ database/migrations/0001_01_01_000005_create_perfilesUsuarios_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
             $table->string('nombre_PerfilUsuario', 50);
             //$table->timestamps(); //created_at updated_at
             $table->timestamp('created_at')->default(DB::raw('(CURRENT_TIMESTAMP - INTERVAL 5 HOUR)'));
-            $table->timestamp('updated_at')->nullable();	
+            $table->timestamp('updated_at')->nullable();
             $table->softDeletes(); //deleted_at
         });
     }
