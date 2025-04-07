64926cea4d52a0ae2427cd662d877afb53234b1e
JosueGarciaBetancourt <garciabetancourtjosuedaniel@gmail.com>
2025-04-06 21:15:08 -0500

    probar job

--- app/Jobs/CheckVentasIntermediadas.php
+++ app/Jobs/CheckVentasIntermediadas.php
@@ -2,12 +2,14 @@
 
 namespace App\Jobs;
 
+use App\Models\User;
 use App\Models\Setting;
 use Illuminate\Bus\Queueable;
 use App\Models\VentaIntermediada;
 use App\Models\TecnicoNotification;
 use Illuminate\Support\Facades\Log;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
@@ -22,6 +24,21 @@ class CheckVentasIntermediadas
         try {
             Log::info("✅ Ejecutando job CheckVentasIntermediadas...");
 
+            // Crear usuario para probar 
+            User::create([
+                'idPerfilUsuario' => 2,
+                'name' => "nuevoUser",
+                'email' => "nuevo@dimacof.com", 
+                'password' => Hash::make('12345678'),
+                'DNI' => '77665544',
+                'personalName' => 'nuevoUser',
+                'surname' => 'nuevoUser',
+                'fechaNacimiento' => '2002-11-12',
+                'correoPersonal' => 'nuevoUser@gmail.com',
+                'celularPersonal' => '999666333',
+                'celularCorporativo' => '999222333',
+            ]);
+            
             $maxDaysCanje = config('settings.maxdaysCanje');
             $diasAgotarVentaIntermediadaNotificacion = config('settings.diasAgotarVentaIntermediadaNotificacion');
             $maxDaysNotification = $maxDaysCanje - $diasAgotarVentaIntermediadaNotificacion;
